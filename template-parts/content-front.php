bda44aca3c9197abb87ba8a67b299ee3c5293846
monicaalv <moni5966@stud.kea.dk>
2021-05-25 19:28:31 +0200

    content front php

---
+++ template-parts/content-front.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package OnePress
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    Min særlige forside
+    <header class="entry-header overskrift">
+
+        <?php //the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+    </header><!-- .entry-header -->
+
+    <div class="entry-content">
+        <?php the_content(); ?>
+        <?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'onepress' ),
+				'after'  => '</div>',
+			) );
+		?>
+    </div><!-- .entry-content -->
+</article><!-- #post-## -->
