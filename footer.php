d25c68c4557a529c05edc040ba070b1353275c7f
monicaalv <moni5966@stud.kea.dk>
2021-05-25 11:22:24 +0200

    content page php

--- footer.php
+++ footer.php
@@ -41,17 +41,7 @@
     <?php endif; ?>
     <div class="site-info">
         <div class="site-name">
-            <?php if ( has_custom_logo() ) : ?>
-            <div class="site-logo"><?php the_custom_logo(); ?></div>
-            <?php else : ?>
-            <?php if ( get_bloginfo( 'name' ) && get_theme_mod( 'display_title_and_tagline', true ) ) : ?>
-            <?php if ( is_front_page() && ! is_paged() ) : ?>
-            <?php bloginfo( 'name' ); ?>
-            <?php else : ?>
-            <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-            <?php endif; ?>
-            <?php endif; ?>
-            <?php endif; ?>
+
         </div><!-- .site-name -->
         <p>Copyright by DoughGirls</p>
 
