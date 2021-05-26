2f4e46c22814f48883a0515444262cc853cdf48c
monicaalv <moni5966@stud.kea.dk>
2021-05-26 11:17:04 +0200

    filtrereing page-doughnuts

--- page-doughnuts.php
+++ page-doughnuts.php
@@ -24,6 +24,16 @@ do_action( 'onepress_page_before_content' );
                 <h1 id="overskrift">Doughnuts</h1>
                 <section id="doughnut-oversigt"></section>
 
+                <nav class="container">
+                    <div class="center">
+                        <button data-kategori="alle" class="valgt">Alle</button>
+                        <button data-kategori="forretter">Vores Doughnuta</button>
+                        <button data-kategori="hovedretter">Vores Café</button>
+                        <button data-kategori="desserter">Bestil Online</button>
+                        <button data-kategori="drikkevarer">Om Os</button>
+                        <button data-kategori="sideorders">FAQ</button>
+                    </div>
+                </nav>
 
                 <template>
                     <article>
@@ -40,38 +50,53 @@ do_action( 'onepress_page_before_content' );
 
             <script>
                 let doughnuts;
+                let filtrer;
 
 
 
-                const dbUrl = "http://monicaamundsen.com/kea/10_eksamen/doughgirls/wp-json/wp/v2/doughnuts?per_page=100";
+                const dbUrl = "http://monicaamundsen.com/kea/10_eksamen/doughgirls/wp-json/wp/v2/doughnut";
 
-
-
-
-                async function getJson() {
-                    const data = await fetch(dbUrl);
-                    doughnuts = await data.json();
-                    console.log(doughnuts);
-                    /*visDoughnuts();*/
+                function start() {
+                    console.log("start");
+                    const filterKnapper = document.querySelectorAll("nav button");
+                    filterKnapper.forEach(knap => knap.addEventListener("click", filtrerKategorier));
+                    loadJSON();
                 }
 
-                /* function visDoughnuts() {
-                     console.log(doughnuts);
-                     doughnuts.forEach(doughnut => {
-                         const klon = skabelon.cloneNode(true).content;
 
-                         klon.querySelector("h2").textContent = doughnut.title.rendered;
-                         klon.querySelector("img").src = doughnut.billede.guid;
-                         klon.querySelector(".pris").textContent = doughnut.pris;
-                         klon.querySelector(".beskrivelse").textContent = doughnut.beskrivelse;
+                function filtrerKategorier() {
+                    filter = this.dataset.kategori;
 
-                         liste.appendChild(klon);
+                    document.querySelector(".valgt").classList.remove(".valgt"); //fjern klassen valgt
+                    this.classList.add(".valgt") //tilføy classen valgt til den knapp som er trykket på
+                    visMadretter(); //kand funksjonen visMadretter etter det nye filter er sat
 
+                    async function getJson() {
+                        const data = await fetch(dbUrl);
+                        doughnuts = await data.json();
+                        console.log(doughnuts);
+                        /*visDoughnuts();*/
+                    }
 
+                    function visDoughnuts() {
+                        console.log(doughnuts);
+                        doughnuts.forEach(doughnut => {
+                            /*     if (filter == doughnuts.kategori || filter == "alle") {
+                             const klon = skabelon.cloneNode(true);*/
+                            const klon = skabelon.cloneNode(true).content;
+                            klon.querySelector("h2").textContent = doughnut.title.rendered;
+                            klon.querySelector("img").src = doughnut.billede.guid;
+                            klon.querySelector(".pris").textContent = doughnut.pris;
+                            klon.querySelector(".beskrivelse").textContent = doughnut.beskrivelse;
 
-                     })
-                 }*/
-                getJson();
+                            liste.appendChild(klon);
+
+                            /*  }*/
+
+                        })
+                    }
+
+                    getJson();
 
             </script>
 
